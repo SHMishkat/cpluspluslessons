6b9a91bf670be5ae33927b78dd735386ce15f1a2
Sarowar H. Mishkat <mishu.sarowar@gmail.com>
2020-01-18 04:20:52 +0600

    User input

---
+++ userInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num, num1,sum;
+    cout << "Enter 2 numbers : ";
+    cin >> num >> num1;
+    sum = num+num1;
+
+    cout << "summation of the tow number is : " <<sum;
+
+//cin in used to take input from user.
+
+}
